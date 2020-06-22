81fc16a9a342f8bbd8f7256079b6f71512dc72dd
Gabriela Vilaro <gabriela.vilaro@uala.com.ar>
2020-06-22 11:59:06 -0300

    first commit

---
+++ activity_index.py
@@ -0,0 +1,24 @@
+class ActivityIndex:
+
+    def __init__(self, driver):
+        self.driver = driver
+        # al abrir la app aparece un cartel de términos y condiciones que hay que seleccionar para poder continuar
+        self.button_tyc = 'android:id/button1'
+        self.button_clear = 'com.miui.calculator:id/btn_c_s'
+        self.button_seven = 'com.miui.calculator:id/btn_7_s'
+        self.button_two = 'com.miui.calculator:id/btn_2_s'
+        self.button_sum = 'com.miui.calculator:id/btn_plus_s'
+        self.button_equal = 'com.miui.calculator:id/btn_equal_s'
+        # resultado de la operación
+        self.result = 'com.miui.calculator:id/result'
+
+    def push_button(self):
+        self.driver.find_element_by_id(self.button_tyc).click()
+        self.driver.find_element_by_id(self.button_clear).click()
+        self.driver.find_element_by_id(self.button_seven).click()
+        self.driver.find_element_by_id(self.button_sum).click()
+        self.driver.find_element_by_id(self.button_two).click()
+        self.driver.find_element_by_id(self.button_equal).click()
+
+    def get_text(self):
+        return self.driver.find_element_by_id(self.result).text
---
+++ search_object.py
@@ -0,0 +1,26 @@
+from appium import webdriver
+import unittest
+from activity_index import ActivityIndex
+
+class Conection(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = {
+          "platformName": "Android",
+          "platformVersion": "9",
+          "deviceName": "lavender",
+          "automatorName": "UiAutomator2",
+          "appPackage": "com.miui.calculator",
+          "appActivity": "com.miui.calculator.cal.CalculatorActivity"
+        }
+
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', self.driver)
+        self.activity_index = ActivityIndex(self.driver)
+
+
+    def test_one_result(self):
+        self.activity_index.push_button()
+        self.assertEqual(self.activity_index.get_text(), '= 9')
+
+if __name__ == '__main__':
+    unittest.main()
